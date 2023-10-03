5b17eb8d1ad5245fd1662376f82842758b74336f
Ismat-Samadov <ismetsemedov@gmail.com>
2023-10-03 22:35:03 +0400

    check

--- 10_2023/data_preparation_and_modelling/cleaning.py
+++ 10_2023/data_preparation_and_modelling/cleaning.py
@@ -1,18 +1,26 @@
+import re
+
 import pandas as pd
 
 data_1 = pd.read_csv('bina_az_02102023.csv')
 data_2 = pd.read_csv('bina_az_new.csv')
 data_3 = pd.read_csv('bina_az_old.csv')
 frames = pd.concat([data_1, data_2, data_3]).drop_duplicates().dropna()
+frames['is_near_metro'] = (
+        frames['description'].str.contains('m\.', case=False) | frames['description'].str.contains('metro',
+                                                                                                   case=False)).astype(
+    int)
 frames = frames[frames['seller_type'] != 'seller_type']
 frames[['flat', 'total_flat']] = frames['flat_number'].str.split(' / ', expand=True).astype(int)
+remove_non_numeric_and_convert_to_float = lambda value: float(re.sub(r'[^\d.]', '', value)) if value else None
+frames['area_converted'] = frames['area'].apply(remove_non_numeric_and_convert_to_float)
 frames['room_count'] = frames['room_count'].astype(int)
 frames['documents_encoded'] = frames['documents'].map({'var': 1, 'yoxdur': 0})
 frames['is_repair_encoded'] = frames['is_repair'].map({'var': 1, 'yoxdur': 0})
 frames['seller_type_encoded'] = frames['seller_type'].map({'vasitəçi (agent)': 0, 'mülkiyyətçi': 1})
 frames['category_encoded'] = frames['category'].map({'Yeni tikili': 0, 'Köhnə tikili': 1})
 frames['price'] = frames['price'].str.replace(' ', '').astype(int)
-features = ['seller_type_encoded', 'flat', 'total_flat', 'area_converted', 'category_encoded', 'documents_encoded',
-            'is_repair_encoded']
-frames = frames[['seller_type_encoded', 'flat', 'total_flat', 'area_converted', 'category_encoded', 'documents_encoded',
+frames = frames[['is_near_metro', 'seller_type_encoded', 'flat', 'total_flat', 'area_converted', 'category_encoded',
+                 'documents_encoded',
                  'is_repair_encoded', 'price']].drop_duplicates(ignore_index=True)
+frames.to_excel('frames.xlsx', index=False)
---
+++ 10_2023/data_preparation_and_modelling/modelling.py
@@ -0,0 +1,30 @@
+import warnings
+import joblib
+import pandas as pd
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.model_selection import train_test_split
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+data = pd.read_excel('frames.xlsx')
+X = data.drop(columns=['price'])
+y = data['price']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = xgb.XGBRegressor(
+    learning_rate=0.1,
+    n_estimators=100,
+    max_depth=3,
+    objective='reg:squarederror')
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+r2 = r2_score(y_test, y_pred)
+rmse = mean_squared_error(y_test, y_pred, squared=False)
+mae = mean_absolute_error(y_test, y_pred)
+print("Model perforamnce metrics")
+print("-----------------------")
+print(f"R-squared: {r2:.2f}")
+print(f"Root Mean Squared Error: {rmse:.2f}")
+print(f"Mean Absolute Error: {mae:.2f}")
+print("-----------------------")
+joblib.dump(model, 'xgb.pkl')
