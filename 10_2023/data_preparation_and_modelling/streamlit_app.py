f6c6b83501faa293c9547b5601f65f3d06fbfdc1
Ismat-Samadov <ismetsemedov@gmail.com>
2023-10-22 18:58:39 +0400

    streamlit

--- 10_2023/data_preparation_and_modelling/streamlit_app.py
+++ 10_2023/data_preparation_and_modelling/streamlit_app.py
@@ -1,20 +1,26 @@
 import streamlit as st
 import requests
 
-API_URL = "http://localhost:5000/predict"  
-
+# Define the Flask API URL
+API_URL = "http://localhost:5000/predict"
+# Streamlit app title
 st.title("Real Estate Price Prediction App")
 
-is_near_metro = st.number_input("Is Near Metro (0 or 1):", min_value=0, max_value=1, step=1)
-seller_type_encoded = st.number_input("Seller Type Encoded (0 or 1):", min_value=0, max_value=1, step=1)
-flat = st.number_input("Flat:", value=2)
-total_flat = st.number_input("Total Flat:", value=5)
-area_converted = st.number_input("Area (converted):")
-category_encoded = st.number_input("Category Encoded (0 or 1):", min_value=0, max_value=1, step=1)
-documents_encoded = st.number_input("Documents Encoded (0 or 1):", min_value=0, max_value=1, step=1)
-is_repair_encoded = st.number_input("Is Repair Encoded (0 or 1):", min_value=0, max_value=1, step=1)
+# Create a sidebar
+st.sidebar.title("Input Data")
 
-if st.button("Predict"):
+# Create input fields for user data in the sidebar
+is_near_metro = st.sidebar.number_input("Is Near Metro (0 or 1):", min_value=0, max_value=1, step=1)
+seller_type_encoded = st.sidebar.number_input("Seller Type Encoded (0 or 1):", min_value=0, max_value=1, step=1)
+flat = st.sidebar.number_input("Flat:", value=2)
+total_flat = st.sidebar.number_input("Total Flat:", value=5)
+area_converted = st.sidebar.number_input("Area (converted):")
+category_encoded = st.sidebar.number_input("Category Encoded (0 or 1):", min_value=0, max_value=1, step=1)
+documents_encoded = st.sidebar.number_input("Documents Encoded (0 or 1):", min_value=0, max_value=1, step=1)
+is_repair_encoded = st.sidebar.number_input("Is Repair Encoded (0 or 1):", min_value=0, max_value=1, step=1)
+
+# Make a prediction request when a button is clicked
+if st.sidebar.button("Predict"):
     data = {
         "is_near_metro": is_near_metro,
         "seller_type_encoded": seller_type_encoded,
@@ -29,11 +35,10 @@ if st.button("Predict"):
         response = requests.post(API_URL, json=data)
         if response.status_code == 200:
             result = response.json()
-            st.success(f"Predicted Price: {result['predicted_price']:.2f}")
+            st.sidebar.success(f"Predicted Price: {result['predicted_price']:.2f}")
         else:
-            st.error("Prediction request failed.")
+            st.sidebar.error("Prediction request failed.")
     except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
-
+        st.sidebar.error(f"An error occurred: {str(e)}")
 
 # streamlit run streamlit_app.py
